74669aa52ebc81ba9a4f02199c288d14becd374d
Jon Jagger <jon@jaggersoft.com>
2016-07-15 08:26:06 +0100

    added colour.rb file to C#/Moq to try out red/amber/green moving from web image into start-points volume

---
+++ C#/Moq/colour.rb
@@ -0,0 +1,6 @@
+lambda { |output|
+    return :red   if /^Errors and Failures\:/.match(output)
+    return :green if /^Tests run: (\d*), Errors: 0, Failures: 0/.match(output)
+    return :amber
+}
+
