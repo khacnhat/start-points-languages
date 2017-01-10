ef9ff42dabe0d7a39595be165133b44a7c1be8f4
Seb Rose <seb@claysnow.co.uk>
2017-01-10 00:28:39 +0000

    Added Cucumber-JS

---
+++ Javascript/cucumber/README.md
@@ -0,0 +1,5 @@
+# Things we are not happy with
+
+* Node should be version 7, not 4
+* Global cucumber install means require('/usr/lib...') instead of require('cucumber')
+* linting is disabled
---
+++ Javascript/cucumber/hiker_steps.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const assert = require('assert');
+const defineSupportCode = require('/usr/local/lib/node_modules/cucumber').defineSupportCode;
+const hiker = require('./hiker')
+
+const CustomWorld = function () {}
+
+defineSupportCode(function (Cucumber) {
+  let score
+
+  Cucumber.Given(/^the hitch-hiker selects some tiles$/, function () {});
+
+  Cucumber.When(/^they spell (\d+) times (\d+)$/, function (a, b) {
+    score = hiker.answer(parseInt(a), parseInt(b));
+  });
+
+  Cucumber.Then(/^the score is (\d+)$/, function (expectedScore) {
+    assert.equal(score, expectedScore);
+  });
+
+
+  // Cucumber.setWorldConstructor(CustomWorld);
+});
