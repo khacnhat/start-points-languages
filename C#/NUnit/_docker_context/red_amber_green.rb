6edc28ba26d2c5be8d494945960ebeefb25efc20
Jon Jagger <jon@jaggersoft.com>
2017-07-11 22:25:50 +0100

    moved C# NUnit,Moq,Specflow to their own cyber-dojo-language org repos

--- C#/NUnit/_docker_context/red_amber_green.rb
+++
@@ -1,7 +0,0 @@
-
-lambda { |stdout,stderr,status|
-  output = stdout + stderr
-  return :red   if /^Errors and Failures:/.match(output)
-  return :green if /^Tests run: (\d+), Errors: 0, Failures: 0/.match(output)
-  return :amber
-}
