fb85f697627fae2afd81286de3c6fdfbfd37b1d1
Jon Jagger <jon@jaggersoft.com>
2017-07-21 14:25:45 +0100

    moved vhdl-assert to its own cyber-dojo-languages org repo

--- vhdl/assert/_docker_context/red_amber_green.rb
+++
@@ -1,7 +0,0 @@
-
-lambda { |stdout,stderr,status|
-  output = stdout + stderr
-  return :red   if /assertion failed/.match(output)
-  return :amber if /compilation error/.match(output)
-  return :green
-}
