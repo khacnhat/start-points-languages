318b9581026ec067e2275da5adb72ed46abcf3a1
Jon Jagger <jon@jaggersoft.com>
2018-12-18 11:16:34 +0000

    add set-v to travis script

--- sh/travis.sh
+++ sh/travis.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-set -e
+set -ev
 
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
 
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points.XXXXXXXXX)
+readonly MY_IMAGE_NAME=cyberdojo/start-points-languages-test
+readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-languages.git
+
+readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo/start-points/languages/.XXXXXXXXX)
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
+
 cd ${TMP_DIR}
 curl -O ${URL}
 chmod +x ./${SCRIPT}
-
-readonly MY_IMAGE_NAME=cyberdojo/start-point-languages-test
-readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-languages.git
 
 ./${SCRIPT} \
   ${MY_IMAGE_NAME} \
