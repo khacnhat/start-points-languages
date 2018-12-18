7d94350a8eb1c575078c2afe13da5ef6109dedbd
Jon Jagger <jon@jaggersoft.com>
2018-12-18 13:07:52 +0000

    refactoring; capture duplication in script

--- sh/travis.sh
+++ sh/travis.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -ev
 
-# todo: MY_REPO_NAME is this repo
-# passing it as file:///... based url would make things a bit quicker
+readonly MY_TYPE=exercises
 
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
 
-readonly MY_IMAGE_NAME=cyberdojo/start-points-languages-test
-readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-languages.git
+readonly MY_IMAGE_NAME=cyberdojo/start-points-${MY_TYPE}-test
+readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-${MY_TYPE}.git
 
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-languages.XXXXXXXXX)
+readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-${MY_TYPE}.XXXXXXXXX)
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
