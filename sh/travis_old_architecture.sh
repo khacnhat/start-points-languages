867bb127090683a1b7ba35fb30d110ddd75b1409
Jon Jagger <jon@jaggersoft.com>
2018-12-18 13:25:56 +0000

    refactoring; create two scripts, to build old and new architecture

---
+++ sh/travis_old_architecture.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+readonly SCRIPT=cyber-dojo
+readonly ORG=https://raw.githubusercontent.com/cyber-dojo
+readonly URL=${ORG}/commander/master/${SCRIPT}
+
+curl -O ${URL}
+chmod +x ./${SCRIPT}
+
+sudo mkdir /cyber-dojo
+sudo chown -R 19663:19663 /cyber-dojo
+
+readonly LIST=${BASE}/start-points-languages/master/languages_list
+./${SCRIPT} start-point create list_languages --list=${LIST}
+./${SCRIPT} start-point ls
+./${SCRIPT} start-point ls | grep list_languages
+
+npm install travis-ci
+readonly TRIGGER=trigger-build.js
+curl -O ${ORG}/cyber-dojo/master/shared/${TRIGGER}
+node ${TRIGGER} cyber-dojo/web
