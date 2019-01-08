103226e79311a3c5ee51d0b30aff0a0895c2f89e
Jon Jagger <jon@jaggersoft.com>
2019-01-08 06:52:09 +0000

    update step-definition files

--- ruby-cucumber/hiker_stepdefs.rb
+++ ruby-cucumber/hiker_stepdefs.rb
@@ -1,11 +1,11 @@
-Given /^the hitch-hiker selects some tiles$/ do
 
+Given /^the hitch-hiker selects some tiles$/ do
 end
 
 When /^they spell (\d+) times (\d+)$/ do |x, y|
-    @result = answer(x.to_i,y.to_i)
+  @result = answer(x.to_i, y.to_i)
 end
 
 Then /^the score is (\d+)$/ do |answer|
-    expect(answer.to_i).to eq(@result)
+  expect(answer.to_i).to eq(@result)
 end
