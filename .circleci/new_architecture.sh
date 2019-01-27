894cd43cdeb9e729aab62662a6fd0e6c0540e1b8
Jon Jagger <jon@jaggersoft.com>
2019-01-27 09:07:58 +0000

    refactoring; better naming

--- .circleci/new_architecture.sh
+++ .circleci/new_architecture.sh
@@ -3,24 +3,28 @@ set -ev
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-readonly MY_TYPE=languages
-readonly SCRIPT=build_cyber_dojo_start_points_image.sh
-readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
-readonly MY_IMAGE_NAME=cyberdojo/start-points
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-${MY_TYPE}.XXXXXXXXX)
+readonly SCRIPT_NAME=build_cyber_dojo_start_points_image.sh
+readonly SCRIPT_URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT_NAME}
+
+readonly LANGUAGE_LIST=languages_list
+readonly LANGUAGE_LIST_URL=https://raw.githubusercontent.com/cyber-dojo/start-points-languages/master/${LANGUAGE_LIST}
+
+readonly IMAGE_NAME=cyberdojo/start-points
+readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points.XXXXXXXXX)
 
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
 cd ${TMP_DIR}
-curl -O ${URL}
-chmod +x ./${SCRIPT}
+curl -O ${SCRIPT_URL}
+chmod 700 ./${SCRIPT_NAME}
+curl -O ${LANGUAGE_LIST_URL}
 
-./${SCRIPT} \
-    ${MY_IMAGE_NAME} \
-    --${MY_TYPE} \
-      "$(< ${MY_DIR}/../languages_list)" \
+./${SCRIPT_NAME} \
+    ${IMAGE_NAME} \
+    --languages \
+      "$(< ./${LANGUAGE_LIST})" \
     --custom \
-      https://github.com/cyber-dojo/start-points-custom.git     \
+      https://github.com/cyber-dojo/start-points-custom.git \
     --exercises \
-      https://github.com/cyber-dojo/start-points-exercises.git  \
+      https://github.com/cyber-dojo/start-points-exercises.git \
